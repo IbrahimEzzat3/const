72aeaaa3d63a0df57baae64f84188ae3629a81e3
IbrahimEzzat3 <eng.ibrahim.ezzat.03@google.com>
2025-06-25 00:26:42 +0300

    videos

--- client/vite.config.js
+++ client/vite.config.js
@@ -20,12 +20,12 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "http://const-production.up.railway.app",
         changeOrigin: true,
         secure: false,
       },
       "/uploads": {
-        target: "http://localhost:5000",
+        target: "http://const-production.up.railway.app",
         changeOrigin: true,
         secure: false,
       },
